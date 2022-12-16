eeda009857e0a1bddcb74ca2597a8e79834c656a
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-16 02:07:47 -0500

    Update build.sh
    in this library we will currently forgo the example build until all connectors are complete

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,9 +1,2 @@
-echo "Finally something worthy to download"
-cd ../ && go mod tidy && go mod vendor &&  gofmt ./ && golangci-lint run && cd deploy ;
-
-echo "Checking dependencies and formatting..."
-go build -o bin/ ./../cmd/datastation/;
-
-echo "Default Config Copying to build directory..."
-cp env bin/.env
-echo "Build complete!";
+echo "analizing this golang project"
+cd ../ && go mod tidy && go mod vendor && gofmt ./ && golangci-lint run && cd deploy;
